4e54d139245dd73d140d2610b872f2656152af40
Yash Zade <yashmzade@gmail.com>
2026-02-04 22:38:48 +0530

    Modify risk nft to link it with ens as well as address

--- contracts/src/RiskNFT.sol
+++ contracts/src/RiskNFT.sol
@@ -9,6 +9,10 @@ interface IERC5192 {
     function locked(uint256 tokenId) external view returns (bool);
 }
 
+interface IENSRegistry {
+    function owner(bytes32 node) external view returns (address);
+}
+
 contract RiskNFT is ERC721, IERC5192 {
     struct RiskProfile {
         uint8 lowPct;
@@ -16,105 +20,149 @@ contract RiskNFT is ERC721, IERC5192 {
         uint8 highPct;
     }
 
+    enum IdentityType {
+        ADDRESS,
+        ENS
+    }
+
+    struct Identity {
+        IdentityType kind;
+        address addr; // if ADDRESS
+        bytes32 ensNode; // if ENS
+    }
+
     uint256 private _tokenIdCounter;
+
     mapping(uint256 => RiskProfile) private _riskProfiles;
-    mapping(address => uint256) private _ownerToTokenId;
+    mapping(uint256 => Identity) private _identity;
+
+    mapping(address => uint256) private _addressToTokenId;
+    mapping(bytes32 => uint256) private _ensToTokenId;
+
+    IENSRegistry public immutable ens;
 
     event RiskProfileMinted(
-        address indexed owner,
-        uint256 tokenId,
+        uint256 indexed tokenId,
         uint8 lowPct,
         uint8 medPct,
         uint8 highPct
     );
+
     event RiskProfileUpdated(
-        address indexed owner,
-        uint256 tokenId,
+        uint256 indexed tokenId,
         uint8 lowPct,
         uint8 medPct,
         uint8 highPct
     );
 
-    constructor() ERC721("Aura Risk Profile", "AURA-RISK") {}
+    constructor(address ensRegistry) ERC721("Aura Risk Profile", "AURA-RISK") {
+        ens = IENSRegistry(ensRegistry);
+    }
 
-    function mint(
+    function mintWithAddress(
         uint8 lowPct,
         uint8 medPct,
         uint8 highPct
     ) external returns (uint256) {
-        require(
-            _ownerToTokenId[msg.sender] == 0,
-            "RiskNFT: user already has NFT"
-        );
-        require(
-            lowPct + medPct + highPct == 100,
-            "RiskNFT: percentages must sum to 100"
-        );
+        require(_addressToTokenId[msg.sender] == 0, "Already has profile");
+        require(lowPct + medPct + highPct == 100, "Invalid split");
 
         _tokenIdCounter++;
         uint256 tokenId = _tokenIdCounter;
 
-        _riskProfiles[tokenId] = RiskProfile({
-            lowPct: lowPct,
-            medPct: medPct,
-            highPct: highPct
+        _riskProfiles[tokenId] = RiskProfile(lowPct, medPct, highPct);
+
+        _identity[tokenId] = Identity({
+            kind: IdentityType.ADDRESS,
+            addr: msg.sender,
+            ensNode: bytes32(0)
         });
 
-        _ownerToTokenId[msg.sender] = tokenId;
-        _safeMint(msg.sender, tokenId);
+        _addressToTokenId[msg.sender] = tokenId;
+
+        _safeMint(address(this), tokenId);
 
         emit Locked(tokenId);
-        emit RiskProfileMinted(msg.sender, tokenId, lowPct, medPct, highPct);
+        emit RiskProfileMinted(tokenId, lowPct, medPct, highPct);
+
+        return tokenId;
+    }
+
+    function mintWithENS(
+        bytes32 ensNode,
+        uint8 lowPct,
+        uint8 medPct,
+        uint8 highPct
+    ) external returns (uint256) {
+        require(ensNode != bytes32(0), "Invalid ENS node");
+        require(ens.owner(ensNode) == msg.sender, "Not ENS owner");
+        require(_ensToTokenId[ensNode] == 0, "ENS already used");
+        require(lowPct + medPct + highPct == 100, "Invalid split");
+
+        _tokenIdCounter++;
+        uint256 tokenId = _tokenIdCounter;
+
+        _riskProfiles[tokenId] = RiskProfile(lowPct, medPct, highPct);
+
+        _identity[tokenId] = Identity({
+            kind: IdentityType.ENS,
+            addr: address(0),
+            ensNode: ensNode
+        });
+
+        _ensToTokenId[ensNode] = tokenId;
+
+        _safeMint(address(this), tokenId);
+
+        emit Locked(tokenId);
+        emit RiskProfileMinted(tokenId, lowPct, medPct, highPct);
 
         return tokenId;
     }
 
     function updateRiskProfile(
+        uint256 tokenId,
         uint8 lowPct,
         uint8 medPct,
         uint8 highPct
     ) external {
-        uint256 tokenId = _ownerToTokenId[msg.sender];
-        require(tokenId != 0, "RiskNFT: user has no NFT");
-        require(
-            lowPct + medPct + highPct == 100,
-            "RiskNFT: percentages must sum to 100"
-        );
+        require(_ownerOf(tokenId) != address(0), "Invalid token");
+        require(_isAuthorized(tokenId), "Not authorized");
+        require(lowPct + medPct + highPct == 100, "Invalid split");
 
-        _riskProfiles[tokenId] = RiskProfile({
-            lowPct: lowPct,
-            medPct: medPct,
-            highPct: highPct
-        });
+        _riskProfiles[tokenId] = RiskProfile(lowPct, medPct, highPct);
 
-        emit RiskProfileUpdated(msg.sender, tokenId, lowPct, medPct, highPct);
+        emit RiskProfileUpdated(tokenId, lowPct, medPct, highPct);
     }
 
     function getRiskProfile(
-        address user
+        uint256 tokenId
     ) external view returns (RiskProfile memory) {
-        uint256 tokenId = _ownerToTokenId[user];
-        require(tokenId != 0, "RiskNFT: user has no NFT");
+        require(_ownerOf(tokenId) != address(0), "Invalid token");
         return _riskProfiles[tokenId];
     }
 
-    function getMyRiskProfile() external view returns (RiskProfile memory) {
-        uint256 tokenId = _ownerToTokenId[msg.sender];
-        require(tokenId != 0, "RiskNFT: you have no NFT");
-        return _riskProfiles[tokenId];
+    function getTokenIdByAddress(address user) external view returns (uint256) {
+        return _addressToTokenId[user];
     }
 
-    function hasProfile(address user) external view returns (bool) {
-        return _ownerToTokenId[user] != 0;
-    }
-
-    function getTokenId(address user) external view returns (uint256) {
-        return _ownerToTokenId[user];
+    function getTokenIdByENS(bytes32 ensNode) external view returns (uint256) {
+        return _ensToTokenId[ensNode];
     }
 
     function locked(uint256 tokenId) external view override returns (bool) {
-        require(_ownerOf(tokenId) != address(0), "RiskNFT: invalid token ID");
+        require(_ownerOf(tokenId) != address(0), "Invalid token");
         return true;
+    }
+
+    function _isAuthorized(uint256 tokenId) internal view returns (bool) {
+        Identity memory id = _identity[tokenId];
+
+        if (id.kind == IdentityType.ADDRESS) {
+            return msg.sender == id.addr;
+        }
+
+        return ens.owner(id.ensNode) == msg.sender;
     }
 
     function _update(
@@ -123,29 +171,23 @@ contract RiskNFT is ERC721, IERC5192 {
         address auth
     ) internal override returns (address) {
         address from = _ownerOf(tokenId);
-        require(
-            from == address(0) || to == address(0),
-            "RiskNFT: token is soulbound"
-        );
 
-        if (to == address(0) && from != address(0)) {
-            delete _ownerToTokenId[from];
-        }
+        require(from == address(0) || to == address(0), "Soulbound");
 
         return super._update(to, tokenId, auth);
     }
 
     function approve(address, uint256) public pure override {
-        revert("RiskNFT: token is soulbound");
+        revert("Soulbound");
     }
 
     function setApprovalForAll(address, bool) public pure override {
-        revert("RiskNFT: token is soulbound");
+        revert("Soulbound");
     }
 
     function supportsInterface(
         bytes4 interfaceId
-    ) public view virtual override returns (bool) {
+    ) public view override returns (bool) {
         return
             interfaceId == type(IERC5192).interfaceId ||
             super.supportsInterface(interfaceId);
